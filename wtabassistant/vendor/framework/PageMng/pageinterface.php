089b143130b05bb21f138fe4e6029380e9ad710d
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-04-09 15:29:47 +0200

    Add files via upload

---
+++ wtabassistant/vendor/framework/PageMng/pageinterface.php
@@ -0,0 +1,30 @@
+<?php
+class pageinterface {
+
+    private $application_config = null;
+
+    public function table($data, $columns){
+        $_html = '<table>';
+        foreach ($data as $row){
+            $_html .= '<tr>';
+            foreach ($columns as $clm){
+                $_html .= '<td>'.$row[$clm].'</td>';
+            }
+            $_html .= '</tr>';
+        }
+        $_html .= '</table>';
+    }
+    public function __construct($application_config) {
+        if(isset($application_config)){
+            $this->application_config = $application_config;
+        }else{
+            die('no $application_config');
+        }
+    }
+    public function _get_application_configs(){
+        if(isset($this->application_config)){
+            return $this->application_config;
+        }
+    }
+    
+}
