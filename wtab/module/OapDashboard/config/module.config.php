51352e60ba726475011232f6e16d6eec37d6b77b
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-05-04 11:18:22 +0200

    Update module.config.php

--- wtab/module/OapDashboard/config/module.config.php
+++ wtab/module/OapDashboard/config/module.config.php
@@ -18,6 +18,26 @@ return [
                     ],
                 ],
             ],
+            'dashboard__index' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/dashboard/index/index',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'dashboard__saveform' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/dashboard/index/saveform',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'saveform',
+                    ],
+                ],
+            ],
 
             //# Menu
             'menuoap' => [
@@ -39,24 +59,15 @@ return [
             
         ],
     ],
-
-    // The 'access_filter' key is used by the User module to restrict or permit
-    // access to certain controller actions for unauthorized visitors.
+    //#TODO
+    /*
     'access_filter' => [
         'options' => [
-            // The access filter can work in 'restrictive' (recommended) or 'permissive'
-            // mode. In restrictive mode all controller actions must be explicitly listed 
-            // under the 'access_filter' config key, and access is denied to any not listed 
-            // action for not logged in users. In permissive mode, if an action is not listed 
-            // under the 'access_filter' key, access to it is permitted to anyone (even for 
-            // not logged in users. Restrictive mode is more secure and recommended to use.
             'mode' => 'restrictive'
         ],
         'controllers' => [
             Controller\IndexController::class => [
-                // Allow anyone to visit "index" and "about" actions
                 ['actions' => [], 'allow' => '*'],
-                // Allow authorized users to visit "settings" action
                 ['actions' => ['index', 'categorie', 'prodotto'], 'allow' => '@']
             ],
         ]
@@ -66,16 +77,6 @@ return [
             Service\NavManager::class => Service\Factory\NavManagerFactory::class,
         ],
     ],
-//    'view_helpers' => [
-//        'factories' => [
-//            View\Helper\Menu::class => View\Helper\Factory\MenuFactory::class,
-//            View\Helper\Breadcrumbs::class => InvokableFactory::class,
-//        ],
-//        'aliases' => [
-//            'mainMenu' => View\Helper\Menu::class,
-//            'pageBreadcrumbs' => View\Helper\Breadcrumbs::class,
-//        ],
-//    ],
     'view_manager' => [
         'template_path_stack' => [
             'oap' => __DIR__ . '/../view',
@@ -84,23 +85,6 @@ return [
             'ViewJsonStrategy',
         ],
     ],
-//    'view_manager' => [
-//        'display_not_found_reason' => true,
-//        'display_exceptions'       => true,
-//        'doctype'                  => 'HTML5',
-//        'not_found_template'       => 'error/404',
-//        'exception_template'       => 'error/index',
-//        'template_map' => [
-//            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-//            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-//            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-//            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-//        ],
-//        'template_path_stack' => [
-//            __DIR__ . '/../view',
-//        ],
-//    ],
-    // The following key allows to define custom styling for FlashMessenger view helper.
     'view_helper_config' => [
         'flashmessenger' => [
             'message_open_format'      => '<div%s><ul><li>',
@@ -108,4 +92,5 @@ return [
             'message_separator_string' => '</li><li>'
         ]
     ],
+    */
 ];
