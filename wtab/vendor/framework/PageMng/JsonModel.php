ad7d08144e99694e8c1d63a8919e7ba6fb15132d
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-05-10 19:29:14 +0200

    Update JsonModel.php

--- wtab/vendor/framework/PageMng/JsonModel.php
+++ wtab/vendor/framework/PageMng/JsonModel.php
@@ -8,5 +8,11 @@ class JsonModel {
     }
     public function __construct($_) {
         $this->_ = $_;
+
+        header("Content-Type: application/json");
+        echo json_encode(
+            $_
+        );
+        die();
     }
 }
