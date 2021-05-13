1a6187ef843905b85cf229186e1117f4e0c3a856
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-05-13 18:27:35 +0200

    Update datamng_crud.js

--- wtabassistant/public/js/OapDatamng/datamng_crud.js
+++ wtabassistant/public/js/OapDatamng/datamng_crud.js
@@ -16,6 +16,9 @@ $( document ).ready(function() {
             $('#tablecontent_generated__query').html(data.selectjoin);
             $('#tablecontent_generated__html').html(data.html_getdata);
             $('#tablecontent_generated__phpview').html(data.php_getdata);
+            $('#tablecontent_generated__html_HTMLviewfield').html(data.html_HTMLviewfield);
+            $('#tablecontent_generated__html_JSviewfield').html(data.html_JSviewfield);
+            
             $('#tablecontent_generated__jsview').html(data.js_getdata);
 
             //# Edit
